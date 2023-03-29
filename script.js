929dc974a5468878e68f0eaa61f515b85721c6ab
Badmus Eniola <115708246+somod-gif@users.noreply.github.com>
2023-03-29 14:23:13 +0100

    work done

--- script.js
+++ script.js
@@ -14,10 +14,23 @@ textArea.addEventListener("input", function () {
   wordCount.value = clearWords.length;
 });
 function mode() {
-  if (document.body.style.background = "white") {
-    document.body.style.background = "black"
-    document.body.style.color = "white"
-    document.getElementById("char").style.color = "white"
-    document.getElementById("word").style.color = "white"
+  if (document.body.style.backgroundColor == "white") {
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
+    document.getElementById("char").style.color = "white";
+    document.getElementById("word").style.color = "white";
+  } else {
+    document.body.style.backgroundColor = "white";
+    document.body.style.color = "black";
+    document.getElementById("char").style.color = "black";
+    document.getElementById("word").style.color = "black";
   }
 }
+// function mode() {
+//     if (document.body.style.background = "white") {
+//         document.body.style.background = "black"
+//         document.body.style.color = "white"
+//         document.getElementById("char").style.color = "white"
+//         document.getElementById("word").style.color = "white"
+//     }
+// }
